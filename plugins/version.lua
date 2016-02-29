e99c3ae5beb903948c664458eb97aca7050a4bb9
shayanchabok007 <shayanchabok5@gmail.com>
2016-02-29 20:43:48 +0000

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,31 @@
+do
+
+function run(msg, matches)
+  return '✅tiger_antispam v2✅'.. VERSION .. [[ 
+ 
+  Developer : @shayan31323
+  
+  Channel : @tiger_channel
+  
+  Open git : https://github.com/shayanchabok007/antispam.teleseed
+  
+ T I G E R🌏 V2
+  
+  All rights reserved.
+  __________________]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows bot version",
+  patterns = {
+    "^!version$"
+  }, 
+  run = run 
+}
+
+end
+--Copyright and edit; @behroozyaghi
+--Persian Translate; @behroozyaghi
+--ch : @nod32team
+--کپی بدون ذکر منبع حرام است
